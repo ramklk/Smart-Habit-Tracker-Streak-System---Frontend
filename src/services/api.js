e9764f555af9c95d81fb2126544da4816c6cd18b
ramklk <ramprasadvkulkarni@gmail.com>
2026-02-20 17:02:03 +0530

    done

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "https://smart-habit-tracker-streak-system-b.vercel.app/",
+    "https://smart-habit-tracker-streak-system-b.vercel.app",
 });
 
 API.interceptors.request.use((req) => {
