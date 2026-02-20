67f89e668fc8a1ae51fae474faa7e8b8077f3443
ramklk <ramprasadvkulkarni@gmail.com>
2026-02-20 17:06:17 +0530

    fixxxxxxxxxxxx

--- src/services/api.js
+++ src/services/api.js
@@ -2,15 +2,24 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "https://smart-habit-tracker-streak-system-b.vercel.app",
+    process.env.REACT_APP_API_URL ||
+    "http://localhost:5000/api",
 });
 
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
+// Attach JWT automatically
+API.interceptors.request.use(
+  (req) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      req.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return req;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return req;
-});
+);
 
 export default API;
